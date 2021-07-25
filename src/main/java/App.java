a2f415bca61000a3f82ceecaa4ed150375d65580
Denisov Sergey <sedenisov@mail.ru>
2021-07-25 12:14:09 +0300

    Init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+        boolean b1 = bean == bean1;
+        boolean b2 = catBean == catBean1;
+        System.out.println(b1);
+        System.out.println(b2);
     }
 }
